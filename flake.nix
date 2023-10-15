9430e9dfdfdb161825c58628497381e5805143d7
Defelo <elodef42@gmail.com>
2023-10-15 02:08:49 +0200

    Remove fenix and naersk

--- flake.nix
+++ flake.nix
@@ -1,50 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-23.05";
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    naersk = {
-      url = "github:nix-community/naersk";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
   outputs = {
     self,
     nixpkgs,
-    fenix,
-    naersk,
     ...
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
-    target = "x86_64-unknown-linux-musl";
-    toolchain = with fenix.packages.${system};
-      combine [
-        stable.rustc
-        stable.cargo
-        targets.${target}.stable.rust-std
-      ];
-    naersk' = naersk.lib.${system}.override {
-      cargo = toolchain;
-      rustc = toolchain;
-    };
   in {
-    packages.${system} = {
+    packages.${system} = let
+      inherit (fromTOML (builtins.readFile ./Cargo.toml)) package;
+    in {
       default = self.packages.${system}.nginx-keycloak;
-      nginx-keycloak = naersk'.buildPackage {
-        src = pkgs.stdenvNoCC.mkDerivation {
-          name = "nginx-keycloak-src";
-          phases = ["installPhase"];
-          installPhase = ''
-            mkdir $out
-            ln -s ${./Cargo.toml} $out/Cargo.toml
-            ln -s ${./Cargo.lock} $out/Cargo.lock
-            ln -s ${./src} $out/src
-          '';
-        };
-        CARGO_BUILD_TARGET = target;
+      nginx-keycloak = pkgs.rustPlatform.buildRustPackage {
+        pname = package.name;
+        version = package.version;
+        src = ./.;
+        cargoLock.lockFile = ./Cargo.lock;
+        doCheck = false;
       };
     };
     nixosModules = {
